fa9433903e3db8a002d7286d6a239fab83a950fd
JoaquinEspino <joaquin.espino@obf.ateneo.edu>
2023-05-06 16:41:18 +0800

    Coins Functionality and Clothes Assets

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -5,19 +5,35 @@ using UnityEngine;
 public class InventoryManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject[] ItemList;
     public Transform panel;
+    public GameObject ItemPrefab;
+    public List<GameObject> Items = new List<GameObject>();
     void Start()
     {
-        foreach (GameObject obj in ItemList)
-        {
-            Instantiate(obj, panel);
-        }
+        RefreshItems();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void RemoveItem(GameObject item)
+    {
+        Items.Remove(item);
+    }
+
+    public void RefreshItems()
+    {
+        foreach (Transform item in panel)
+        {
+            Destroy(item.gameObject);
+        }
+
+       foreach(var item in Items)
+        {
+            GameObject obj = Instantiate(ItemPrefab, panel);
+        }
     }
 }
--- Assets/Scripts/ItemAttributes.cs
+++ Assets/Scripts/ItemAttributes.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class ItemAttributes : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject thisItem;
+    public GameObject coinsManager;
+    public GameObject inventoryManager;
+    public int sellCost;
     void Start()
     {
-        
+        sellCost = 3;
+        coinsManager = GameObject.FindWithTag("CoinManager");
+        inventoryManager = GameObject.FindWithTag("InventoryManager");
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void SellItem()
+    {
+        coinsManager.GetComponent<CoinsManager>().coinCount += sellCost;
+       
+        inventoryManager.GetComponent<InventoryManager>().RefreshItems();
+        inventoryManager.GetComponent<InventoryManager>().RemoveItem(thisItem);
     }
 }
