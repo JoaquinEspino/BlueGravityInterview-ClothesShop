9ae40bf5436c45d3593adf98386e146e5b7529ad
JoaquinEspino <joaquin.espino@obf.ateneo.edu>
2023-05-06 23:44:06 +0800

    Sorting Layers and Unequiping Clothes

--- Assets/Scripts/Wearitem.cs
+++ Assets/Scripts/Wearitem.cs
@@ -8,12 +8,14 @@ public class Wearitem : MonoBehaviour
     public SpriteRenderer hats, shirts, pants, scarfs;
     // Start is called before the first frame update
     public List<SpriteRenderer> clothesHolder;
+    bool buttonFlipper;
     void Start()
     {
         hats = GameObject.FindWithTag("Hats").GetComponent<SpriteRenderer>();
         shirts = GameObject.FindWithTag("Shirts").GetComponent<SpriteRenderer>();
         pants = GameObject.FindWithTag("Pants").GetComponent<SpriteRenderer>();
         scarfs = GameObject.FindWithTag("Scarfs").GetComponent<SpriteRenderer>();
+        buttonFlipper = false;
     }
 
     // Update is called once per frame
@@ -24,23 +26,39 @@ public class Wearitem : MonoBehaviour
 
     public void WearItem()
     {
-        if(gameObject.GetComponent<ItemAttributes>().itemTag == "hats")
+        if(!buttonFlipper)
         {
-            clothesHolder.Add(hats); 
-        }
-        else if (gameObject.GetComponent<ItemAttributes>().itemTag == "shirts")
-        {
-            clothesHolder.Add(shirts);
-        }
-        else if (gameObject.GetComponent<ItemAttributes>().itemTag == "pants")
-        {
-            clothesHolder.Add(pants);
-        }
-        else if (gameObject.GetComponent<ItemAttributes>().itemTag == "scarfs")
-        {
-            clothesHolder.Add(scarfs);
-        }
+            if (gameObject.GetComponent<ItemAttributes>().itemTag == "hats")
+            {
+                clothesHolder.Add(hats);
+            }
+            else if (gameObject.GetComponent<ItemAttributes>().itemTag == "shirts")
+            {
+                clothesHolder.Add(shirts);
+            }
+            else if (gameObject.GetComponent<ItemAttributes>().itemTag == "pants")
+            {
+                clothesHolder.Add(pants);
+            }
+            else if (gameObject.GetComponent<ItemAttributes>().itemTag == "scarfs")
+            {
+                clothesHolder.Add(scarfs);
+            }
 
-        clothesHolder[0].sprite = gameObject.GetComponent<ItemAttributes>().chosenSprite;
+            clothesHolder[0].sprite = gameObject.GetComponent<ItemAttributes>().chosenSprite;
+            gameObject.GetComponent<Image>().color = Color.gray;
+            
+        }
+        if(buttonFlipper)
+        {
+            clothesHolder[0].sprite = null;
+
+            gameObject.GetComponent<Image>().color = Color.white;
+            
+        }
+        buttonFlipper = !buttonFlipper;
+
+
+
     }
 }
